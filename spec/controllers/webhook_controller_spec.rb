8dd71713ee0b995bdf8e0a755e3b9cbba948f743
Matheus Vinhas <matheusfvinhas@gmail.com>
2018-06-25 10:57:55 -0300

    add 404 test GET /issues/:id/events

--- spec/controllers/webhook_controller_spec.rb
+++ spec/controllers/webhook_controller_spec.rb
@@ -4,13 +4,21 @@ require 'rails_helper'
 
 RSpec.describe WebhookController, type: :request do
   let!(:event) { create(:event) }
+  let(:issue_number) { event.issue.number }
 
   describe 'GET /issues/:id/events' do
-    before { get "/issues/#{event.issue.number}/events" }
+    before { get "/issues/#{issue_number}/events" }
 
     context 'when events exists' do
       it 'returns status code 200' do
         expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when events not exists' do
+      let(:issue_number) { 100 }
+      it 'returns status code 404' do
+        expect(response).to have_http_status(404)
       end
     end
   end
@@ -19,10 +27,6 @@ RSpec.describe WebhookController, type: :request do
     before { get '/statistics' }
 
     context 'when statistics exists' do
-      it 'returns status code 200' do
-        expect(response).to have_http_status(200)
-      end
-
       it 'returns status code 200' do
         expect(response).to have_http_status(200)
       end
